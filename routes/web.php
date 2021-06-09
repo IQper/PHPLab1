162a94484c594dcf0829371cfe615ce98d15afc2
IQper <you.us.be@bk.ru>
2021-06-09 13:22:18 +0500

    authorization done

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/main', function () {
+    return view('main');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
