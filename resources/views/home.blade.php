97d4ec289de1bdb9e9433002f10189a70e39f709
IQper <you.us.be@bk.ru>
2021-06-27 23:02:25 +0500

    finale

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        @for ($i = 0; $i < 100; $i++)
-            <br>
-            The current value is {{ $i }}
-        @endfor
+
         <div class="col-md-8">
             <h1>Главная</h1>
         </div>
